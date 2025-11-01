fbb6857a377b7fa765a048ee3cc52ac249816530
flashcodes-themayankjha <the.mayank.k.jha@gmail.com>
2025-11-01 16:52:42 +0530

    feat: Setup parser, UI and debug command

--- lua/fknb/init.lua
+++ lua/fknb/init.lua
@@ -1,15 +1,14 @@
 local M = {}
 
 function M.setup()
+  -- Filetype + icons
   require("fknb.utils.fdetect")
   require("fknb.utils.icon").setup()
 
   -- Treesitter parser config
   local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
   parser_config.fknb = {
     install_info = {
-      -- MUST be your repo, not local path
       url = "https://github.com/flashcodes-themayankjha/tree-sitter-fknb",
       files = { "src/parser.c" },
       branch = "main",
@@ -17,10 +16,23 @@ function M.setup()
     filetype = "fknb",
   }
 
-  -- Only needed on Neovim 0.10+ (NOT older versions)
+  -- Needed for Neovim 0.10+
   if vim.treesitter.language then
     vim.treesitter.language.register("fknb", "fknb")
   end
+
+  -- Parser + UI
+  local parser = require("fknb.core.parser")
+  local ui = require("fknb.ui.cell_ui")
+
+  -- Debug command
+  vim.api.nvim_create_user_command("FKNBParse", function()
+    local cells = parser.parse()
+    print(vim.inspect(cells))
+  end, {})
+
+  -- Attach smoothing UI behavior
+  ui.attach_autocmd()
 end
 
 return M
