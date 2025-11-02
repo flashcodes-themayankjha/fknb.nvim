3d4bc628686f645af0cdc023cc80bb125f39efe0
flashcodes-themayankjha <the.mayank.k.jha@gmail.com>
2025-11-02 21:51:13 +0530

    Feat(state): Expand state module to manage kernel, cells, and messages

--- lua/fknb/utils/state.lua
+++ lua/fknb/utils/state.lua
@@ -1,8 +1,39 @@
+-- lua/fknb/utils/state.lua
+
 local M = {
+  kernel = {
+    name = nil,
+    connection_file = nil,
+    session_id = nil,
+    pid = nil,
+
+    transport = nil, -- tcp/zmq
+    ports = {
+      hb = nil,
+      control = nil,
+      shell = nil,
+      stdin = nil,
+      iopub = nil,
+    },
+
+    running = false,
+  },
+
   cells = {},
+  prev_cells = {},
+
+  --- Active / last executed cell
   current_cell = nil,
-  kernel = nil,
+
+  --- Kernel message queues
+  messages = {
+    shell = {},
+    iopub = {},
+    stdin = {},
+  },
+
+  --- Execution counters
+  exec_count = 0,
 }
 
 return M
-
